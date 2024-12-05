0928ce353174412a6d33a0926e203e71ed52eb15
AlexNguyen <163830781+minhquangAlex@users.noreply.github.com>
2024-12-05 15:03:12 +0700

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import App from '@/App.vue';
+import AdminPage from '@/components/AdminPage.vue';
+import EmployeePage from '@/components/EmployeePage.vue';
 import AdminLogin from '@/components/Login/AdminLogin.vue';
 import StudentLogin from '@/components/Login/StudentLogin.vue';
 import WaitingLogin from '@/components/Login/WaitingLogin.vue';
-import AdminPage from '@/components/AdminPage.vue';
+import Orderpage from '@/components/OrderPage.vue';
+import ProductPage from '@/components/ProductPage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
@@ -11,6 +14,9 @@ const routes = [
   {path:'/login/admin-login',name:'AdminLogin', component: AdminLogin },
   {path:'/login/user-login',name:'StudentLogin', component:StudentLogin },
   {path:'/login/admin-page',name:'AdminPage', component:AdminPage },
+  {path:'/admin/products',name:'ProductPage', component:ProductPage },
+  {path:'/admin/orders',name:'OrderPage', component:Orderpage },
+  {path:'/admin/employees',name:'EmployeePage', component:EmployeePage },
 ];
 
 const router = createRouter({
